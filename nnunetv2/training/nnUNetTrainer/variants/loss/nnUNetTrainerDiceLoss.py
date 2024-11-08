c537f9535dc65e6bdf0e9ee83a5eea342b99eeb1
Alireza Hokmabadi <a.hokmabadi.ee@gmail.com>
2024-11-08 17:58:58 +0000

    Add nnUNetTrainer with DiceCELoss, no smoothing, 250 epochs

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerDiceLoss.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerDiceLoss.py
@@ -56,3 +56,10 @@ class nnUNetTrainerDiceCELoss_noSmooth(nnUNetTrainer):
         loss = DeepSupervisionWrapper(loss, weights)
         return loss
 
+
+class nnUNetTrainerDiceCELoss_noSmooth_250epochs(nnUNetTrainerDiceCELoss_noSmooth):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 250
+
