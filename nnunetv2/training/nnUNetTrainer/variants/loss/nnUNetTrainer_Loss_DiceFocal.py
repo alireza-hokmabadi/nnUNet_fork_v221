8f56c09d50f49e16d25a5dbed816b7061ed79651
Alireza Hokmabadi <a.hokmabadi.ee@gmail.com>
2025-09-08 12:14:05 +0100

    Adding new loss

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainer_Loss_DiceFocal.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainer_Loss_DiceFocal.py
@@ -2,11 +2,12 @@
 from nnunetv2.training.loss.dice_loss import DC_and_Focal_loss
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 
-class nnUNetTrainer_Loss_DiceFocal(nnUNetTrainer):
-    def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
-                 unpack_data=True, deterministic=True, fp16=False):
-        super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
-                                              unpack_data, deterministic, fp16)
 
-        self.loss = DC_and_Focal_loss({'batch_dice':self.batch_dice, 'smooth':1e-5,
-        	'do_bg':False}, {'alpha':0.5, 'gamma':2, 'smooth':1e-5})
+class nnUNetTrainer_Loss_DiceFocal(nnUNetTrainer):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, **kwargs):
+        super().__init__(plans, configuration, fold, dataset_json, **kwargs)
+
+        self.loss = DC_and_Focal_loss(
+            {'batch_dice': self.configuration.batch_dice, 'smooth': 1e-5, 'do_bg': False},
+            {'alpha': 0.5, 'gamma': 2, 'smooth': 1e-5}
+        )
