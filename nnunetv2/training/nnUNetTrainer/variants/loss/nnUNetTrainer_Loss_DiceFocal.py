3e39584bf56ac4688851ca574d9ab54e6f29e145
Alireza Hokmabadi <a.hokmabadi.ee@gmail.com>
2025-09-08 12:30:35 +0100

    Adding new loss

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainer_Loss_DiceFocal.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainer_Loss_DiceFocal.py
@@ -25,13 +25,24 @@ from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 
 
 
+# class nnUNetTrainerV2_Loss_DiceFocal(nnUNetTrainerV2):
+#     def __init__(self, plans_file, fold, output_folder=None, dataset_directory=None, batch_dice=True, stage=None,
+#                  unpack_data=True, deterministic=True, fp16=False):
+#         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage,
+#                                               unpack_data, deterministic, fp16)
+
+#         self.loss = DC_and_Focal_loss({'batch_dice':self.batch_dice, 'smooth':1e-5,
+#         	'do_bg':False}, {'alpha':0.5, 'gamma':2, 'smooth':1e-5})
+
+
+
 
 class nnUNetTrainer_Loss_DiceFocal(nnUNetTrainer):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda'), batch_dice=True):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device, batch_dice)
 
         self.loss = DC_and_Focal_loss(
-            {'batch_dice': self.configuration.batch_dice, 'smooth': 1e-5, 'do_bg': False},
+            {'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False},
             {'alpha': 0.5, 'gamma': 2, 'smooth': 1e-5}
         )
 
