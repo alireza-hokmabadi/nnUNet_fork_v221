b90c421e8699cb59741c0325ea491fdfdf5cd1a7
Alireza Hokmabadi <a.hokmabadi.ee@gmail.com>
2025-09-08 12:37:42 +0100

    Adding new loss

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainer_Loss_DiceFocal.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainer_Loss_DiceFocal.py
@@ -37,12 +37,47 @@ from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 
 
 
+# class nnUNetTrainer_Loss_DiceFocal(nnUNetTrainer):
+#     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda'), batch_dice=True):
+#         super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device, batch_dice)
+
+#         self.loss = DC_and_Focal_loss(
+#             {'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False},
+#             {'alpha': 0.5, 'gamma': 2, 'smooth': 1e-5}
+#         )
+
+
 class nnUNetTrainer_Loss_DiceFocal(nnUNetTrainer):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda'), batch_dice=True):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device, batch_dice)
+    def __init__(
+        self,
+        plans: dict,
+        configuration: str,
+        fold: int,
+        dataset_json: dict,
+        unpack_data: bool = True,
+        deterministic: bool = True,
+        fp16: bool = False,
+        enable_deep_supervision: bool = True,
+        output_folder: str = None,
+        dataset_directory: str = None,
+        **unused_kwargs  # optional safety net
+    ):
+        super().__init__(
+            plans=plans,
+            configuration=configuration,
+            fold=fold,
+            dataset_json=dataset_json,
+            unpack_data=unpack_data,
+            deterministic=deterministic,
+            fp16=fp16,
+            enable_deep_supervision=enable_deep_supervision,
+            output_folder=output_folder,
+            dataset_directory=dataset_directory,
+        )
 
         self.loss = DC_and_Focal_loss(
             {'batch_dice': self.batch_dice, 'smooth': 1e-5, 'do_bg': False},
             {'alpha': 0.5, 'gamma': 2, 'smooth': 1e-5}
         )
+
 
