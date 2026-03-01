bbad69071046702c6468e70cbf75007c7a0d1a72
Alireza Hokmabadi <a.hokmabadi.ee@gmail.com>
2026-03-01 10:04:47 +0000

    Edit loss

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerDiceLoss.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerDiceLoss.py
@@ -10,20 +10,19 @@ from nnunetv2.utilities.helpers import softmax_helper_dim1
 
 class nnUNetTrainerDiceLoss(nnUNetTrainer):
     def _build_loss(self):
-        loss = MemoryEfficientSoftDiceLoss(**{'batch_dice': self.configuration_manager.batch_dice,
-                                    'do_bg': self.label_manager.has_regions, 'smooth': 1e-5, 'ddp': self.is_ddp},
-                            apply_nonlin=torch.sigmoid if self.label_manager.has_regions else softmax_helper_dim1)
+        loss = MemoryEfficientSoftDiceLoss(
+            **{
+                'batch_dice': self.configuration_manager.batch_dice,
+                'do_bg': self.label_manager.has_regions,
+                'smooth': 1e-5,
+                'ddp': self.is_ddp
+            },
+            apply_nonlin=torch.sigmoid if self.label_manager.has_regions else softmax_helper_dim1)
 
         deep_supervision_scales = self._get_deep_supervision_scales()
-
-        # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
-        # this gives higher resolution outputs more weight in the loss
         weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
         weights[-1] = 0
-
-        # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
         weights = weights / weights.sum()
-        # now wrap the loss
         loss = DeepSupervisionWrapper(loss, weights)
         return loss
 
@@ -32,27 +31,129 @@ class nnUNetTrainerDiceCELoss_noSmooth(nnUNetTrainer):
     def _build_loss(self):
         # set smooth to 0
         if self.label_manager.has_regions:
-            loss = DC_and_BCE_loss({},
-                                   {'batch_dice': self.configuration_manager.batch_dice,
-                                    'do_bg': True, 'smooth': 0, 'ddp': self.is_ddp},
-                                   use_ignore_label=self.label_manager.ignore_label is not None,
-                                   dice_class=MemoryEfficientSoftDiceLoss)
+            loss = DC_and_BCE_loss(
+                {},
+                {
+                    'batch_dice': self.configuration_manager.batch_dice,
+                    'do_bg': True,
+                    'smooth': 0,
+                    'ddp': self.is_ddp
+                },
+                use_ignore_label=self.label_manager.ignore_label is not None,
+                dice_class=MemoryEfficientSoftDiceLoss
+            )
         else:
-            loss = DC_and_CE_loss({'batch_dice': self.configuration_manager.batch_dice,
-                                   'smooth': 0, 'do_bg': False, 'ddp': self.is_ddp}, {}, weight_ce=1, weight_dice=1,
-                                  ignore_label=self.label_manager.ignore_label,
-                                  dice_class=MemoryEfficientSoftDiceLoss)
+            loss = DC_and_CE_loss(
+                {
+                    'batch_dice': self.configuration_manager.batch_dice,
+                    'smooth': 0,
+                    'do_bg': False,
+                    'ddp': self.is_ddp
+                },
+                {},
+                weight_ce=1,
+                weight_dice=1,
+                ignore_label=self.label_manager.ignore_label,
+                dice_class=MemoryEfficientSoftDiceLoss
+            )
 
         deep_supervision_scales = self._get_deep_supervision_scales()
-
-        # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
-        # this gives higher resolution outputs more weight in the loss
         weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
         weights[-1] = 0
-
-        # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
         weights = weights / weights.sum()
-        # now wrap the loss
+
+        loss = DeepSupervisionWrapper(loss, weights)
+        return loss
+
+
+
+class nnUNetTrainerDiceCELoss_noSmooth_weighted(nnUNetTrainer):
+    def _build_loss(self):
+
+        if self.label_manager.has_regions:
+            loss = DC_and_BCE_loss(
+                {},
+                {
+                    'batch_dice': self.configuration_manager.batch_dice,
+                    'do_bg': True,
+                    'smooth': 0,
+                    'ddp': self.is_ddp
+                },
+                use_ignore_label=self.label_manager.ignore_label is not None,
+                dice_class=MemoryEfficientSoftDiceLoss
+            )
+
+        else:
+            # ---- ADD THIS ----
+            class_weights = torch.tensor(
+                [1.0, 1.0, 1.5],  # bg, endo, epi
+                device=self.device
+            )
+
+            loss = DC_and_CE_loss(
+                {
+                    'batch_dice': self.configuration_manager.batch_dice,
+                    'smooth': 0,
+                    'do_bg': False,
+                    'ddp': self.is_ddp
+                },
+                {'weight': class_weights},  # <-- pass to CE
+                weight_ce=1,
+                weight_dice=1,
+                ignore_label=self.label_manager.ignore_label,
+                dice_class=MemoryEfficientSoftDiceLoss
+            )
+
+        deep_supervision_scales = self._get_deep_supervision_scales()
+        weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+        weights[-1] = 0
+        weights = weights / weights.sum()
+
+        loss = DeepSupervisionWrapper(loss, weights)
+        return loss
+
+
+class nnUNetTrainerDiceCELoss_noSmooth_weighted_250epochs(nnUNetTrainerDiceCELoss_noSmooth_weighted):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 250
+
+
+class nnUNetTrainer_DiceCE_Weighted(nnUNetTrainer):
+    def _build_loss(self):
+        if self.label_manager.has_regions:
+            loss = DC_and_BCE_loss(
+                {},
+                {
+                    'batch_dice': self.configuration_manager.batch_dice,
+                    'do_bg': True,
+                    'smooth': 1e-5,
+                    'ddp': self.is_ddp
+                },
+                use_ignore_label=self.label_manager.ignore_label is not None,
+                dice_class=MemoryEfficientSoftDiceLoss
+            )
+        else:
+            loss = DC_and_CE_loss(
+                {},
+                {
+                    'batch_dice': self.configuration_manager.batch_dice,
+                    'do_bg': False,
+                    'smooth': 1e-5,
+                    'ddp': self.is_ddp
+                },
+                weight_ce=0.7,
+                weight_dice=0.3,
+                ignore_label=self.label_manager.ignore_label,
+                dice_class=MemoryEfficientSoftDiceLoss
+            )
+
+        deep_supervision_scales = self._get_deep_supervision_scales()
+        weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+        weights[-1] = 0
+        weights = weights / weights.sum()
+
         loss = DeepSupervisionWrapper(loss, weights)
         return loss
 
@@ -104,33 +205,6 @@ class nnUNetTrainerDiceCELoss_noSmooth_2000epochs_NoMirroring(nnUNetTrainerDiceC
         mirror_axes = None
         self.inference_allowed_mirroring_axes = None
         return rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes
-
-
-class nnUNetTrainer_DiceCE_Weighted(nnUNetTrainer):
-    def _build_loss(self):
-        if self.label_manager.has_regions:
-            loss = DC_and_BCE_loss({},
-                                    {'batch_dice': self.configuration_manager.batch_dice,
-                                    'do_bg': True, 'smooth': 1e-5, 'ddp': self.is_ddp},
-                                    use_ignore_label=self.label_manager.ignore_label is not None,
-                                    dice_class=MemoryEfficientSoftDiceLoss)
-        else:
-            loss = DC_and_CE_loss({'batch_dice': self.configuration_manager.batch_dice,
-                                    'smooth': 1e-5, 'do_bg': False, 'ddp': self.is_ddp}, {}, weight_ce=0.7, weight_dice=0.3,
-                                  ignore_label=self.label_manager.ignore_label, dice_class=MemoryEfficientSoftDiceLoss)
-
-        deep_supervision_scales = self._get_deep_supervision_scales()
-
-        # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
-        # this gives higher resolution outputs more weight in the loss
-        weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
-        weights[-1] = 0
-
-        # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
-        weights = weights / weights.sum()
-        # now wrap the loss
-        loss = DeepSupervisionWrapper(loss, weights)
-        return loss
 
 
 class nnUNetTrainer_DiceCE_Weighted_250epochs(nnUNetTrainer_DiceCE_Weighted):
