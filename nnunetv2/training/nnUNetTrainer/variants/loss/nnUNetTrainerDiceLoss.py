9fde962cb13d0fe7458951d1372572cab554dce0
Alireza Hokmabadi <a.hokmabadi.ee@gmail.com>
2026-03-01 11:41:37 +0000

    Edit loss

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerDiceLoss.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerDiceLoss.py
@@ -7,7 +7,7 @@ from nnunetv2.training.loss.dice import MemoryEfficientSoftDiceLoss
 from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
 from nnunetv2.utilities.helpers import softmax_helper_dim1
 
-
+#%% ---------------------------------------------------------------------------
 class nnUNetTrainerDiceLoss(nnUNetTrainer):
     def _build_loss(self):
         loss = MemoryEfficientSoftDiceLoss(
@@ -17,19 +17,21 @@ class nnUNetTrainerDiceLoss(nnUNetTrainer):
                 'smooth': 1e-5,
                 'ddp': self.is_ddp
             },
-            apply_nonlin=torch.sigmoid if self.label_manager.has_regions else softmax_helper_dim1)
+            apply_nonlin=torch.sigmoid if self.label_manager.has_regions else softmax_helper_dim1
+        )
 
         deep_supervision_scales = self._get_deep_supervision_scales()
         weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
         weights[-1] = 0
         weights = weights / weights.sum()
+
         loss = DeepSupervisionWrapper(loss, weights)
         return loss
 
-
+#%% ---------------------------------------------------------------------------
 class nnUNetTrainerDiceCELoss_noSmooth(nnUNetTrainer):
     def _build_loss(self):
-        # set smooth to 0
+        # --- set smooth to 0 ---
         if self.label_manager.has_regions:
             loss = DC_and_BCE_loss(
                 {},
@@ -39,18 +41,20 @@ class nnUNetTrainerDiceCELoss_noSmooth(nnUNetTrainer):
                     'smooth': 0,
                     'ddp': self.is_ddp
                 },
+                weight_ce=1,
+                weight_dice=1,
                 use_ignore_label=self.label_manager.ignore_label is not None,
                 dice_class=MemoryEfficientSoftDiceLoss
             )
         else:
             loss = DC_and_CE_loss(
+                {},
                 {
                     'batch_dice': self.configuration_manager.batch_dice,
-                    'smooth': 0,
                     'do_bg': False,
+                    'smooth': 0,
                     'ddp': self.is_ddp
                 },
-                {},
                 weight_ce=1,
                 weight_dice=1,
                 ignore_label=self.label_manager.ignore_label,
@@ -66,14 +70,67 @@ class nnUNetTrainerDiceCELoss_noSmooth(nnUNetTrainer):
         return loss
 
 
+class nnUNetTrainerDiceCELoss_noSmooth_100epochs(nnUNetTrainerDiceCELoss_noSmooth):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 100
 
+
+class nnUNetTrainerDiceCELoss_noSmooth_250epochs(nnUNetTrainerDiceCELoss_noSmooth):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 250
+
+
+class nnUNetTrainerDiceCELoss_noSmooth_500epochs(nnUNetTrainerDiceCELoss_noSmooth):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 500
+
+
+class nnUNetTrainerDiceCELoss_noSmooth_2000epochs(nnUNetTrainerDiceCELoss_noSmooth):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 2000
+
+
+class nnUNetTrainerDiceCELoss_noSmooth_4000epochs(nnUNetTrainerDiceCELoss_noSmooth):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 4000
+
+
+class nnUNetTrainerDiceCELoss_noSmooth_8000epochs(nnUNetTrainerDiceCELoss_noSmooth):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 8000
+
+
+#%% ---------------------------------------------------------------------------
+class nnUNetTrainerDiceCELoss_noSmooth_2000epochs_NoMirroring(nnUNetTrainerDiceCELoss_noSmooth):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 2000
+
+    def configure_rotation_dummyDA_mirroring_and_inital_patch_size(self):
+        rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes = super().configure_rotation_dummyDA_mirroring_and_inital_patch_size()
+        mirror_axes = None
+        self.inference_allowed_mirroring_axes = None
+        return rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes
+
+
+#%% ---------------------------------------------------------------------------
 class nnUNetTrainerFlexibleLoss(nnUNetTrainer):
 
-    # ---- default values (can be overridden) ----
+    # ---- default experiment parameters ----
     class_weights = None
     weight_ce = 1.0
     weight_dice = 1.0
-    num_epochs = 1000
+    num_epochs = 1000   # default
+
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = self.__class__.num_epochs
 
     def _build_loss(self):
 
@@ -107,8 +164,8 @@ class nnUNetTrainerFlexibleLoss(nnUNetTrainer):
             loss = DC_and_CE_loss(
                 {
                     'batch_dice': self.configuration_manager.batch_dice,
-                    'smooth': 0,
                     'do_bg': False,
+                    'smooth': 0,
                     'ddp': self.is_ddp
                 },
                 ce_kwargs,
@@ -126,173 +183,139 @@ class nnUNetTrainerFlexibleLoss(nnUNetTrainer):
         return DeepSupervisionWrapper(loss, weights)
 
 
-class nnUNetTrainer_baseline_250epochs(nnUNetTrainerFlexibleLoss):
+class nnUNetTrainerFlexibleLoss_100ep(nnUNetTrainerFlexibleLoss):
+    class_weights = None
+    weight_ce = 1.0
+    weight_dice = 1.0
+    num_epochs = 100
+
+
+class nnUNetTrainerFlexibleLoss_250ep(nnUNetTrainerFlexibleLoss):
     class_weights = None
     weight_ce = 1.0
     weight_dice = 1.0
     num_epochs = 250
 
 
-class nnUNetTrainerDiceCELoss_noSmooth_weighted(nnUNetTrainer):
-
-    def _build_loss(self):
-
-        # use self.class_weights if defined
-        if hasattr(self, "class_weights") and self.class_weights is not None:
-            class_weights = torch.tensor(
-                self.class_weights,
-                dtype=torch.float32,
-                device=self.device
-            )
-            ce_kwargs = {'weight': class_weights}
-        else:
-            ce_kwargs = {}
-
-        if self.label_manager.has_regions:
-            loss = DC_and_BCE_loss(
-                ce_kwargs,
-                {
-                    'batch_dice': self.configuration_manager.batch_dice,
-                    'do_bg': True,
-                    'smooth': 0,
-                    'ddp': self.is_ddp
-                },
-                weight_ce=1,
-                weight_dice=1,
-                use_ignore_label=self.label_manager.ignore_label is not None,
-                dice_class=MemoryEfficientSoftDiceLoss
-            )
-
-        else:
-            loss = DC_and_CE_loss(
-                {
-                    'batch_dice': self.configuration_manager.batch_dice,
-                    'smooth': 0,
-                    'do_bg': False,
-                    'ddp': self.is_ddp
-                },
-                ce_kwargs,
-                weight_ce=1,
-                weight_dice=1,
-                ignore_label=self.label_manager.ignore_label,
-                dice_class=MemoryEfficientSoftDiceLoss
-            )
-
-        deep_supervision_scales = self._get_deep_supervision_scales()
-        weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
-        weights[-1] = 0
-        weights = weights / weights.sum()
-
-        loss = DeepSupervisionWrapper(loss, weights)
-
-        return loss
+class nnUNetTrainerFlexibleLoss_500ep(nnUNetTrainerFlexibleLoss):
+    class_weights = None
+    weight_ce = 1.0
+    weight_dice = 1.0
+    num_epochs = 500
 
 
-class nnUNetTrainerDiceCELoss_noSmooth_weighted_250epochs(nnUNetTrainerDiceCELoss_noSmooth_weighted):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
-
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-
-        self.num_epochs = 250
-        self.class_weights = [1.0, 1.0, 1.5]
+class nnUNetTrainerFlexibleLoss_250ep_ce2(nnUNetTrainerFlexibleLoss):
+    class_weights = None
+    weight_ce = 2.0
+    weight_dice = 1.0
+    num_epochs = 250
 
 
-class nnUNetTrainer_DiceCE_Weighted(nnUNetTrainer):
-    def _build_loss(self):
-        if self.label_manager.has_regions:
-            loss = DC_and_BCE_loss(
-                {},
-                {
-                    'batch_dice': self.configuration_manager.batch_dice,
-                    'do_bg': True,
-                    'smooth': 1e-5,
-                    'ddp': self.is_ddp
-                },
-                use_ignore_label=self.label_manager.ignore_label is not None,
-                dice_class=MemoryEfficientSoftDiceLoss
-            )
-        else:
-            loss = DC_and_CE_loss(
-                {},
-                {
-                    'batch_dice': self.configuration_manager.batch_dice,
-                    'do_bg': False,
-                    'smooth': 1e-5,
-                    'ddp': self.is_ddp
-                },
-                weight_ce=0.7,
-                weight_dice=0.3,
-                ignore_label=self.label_manager.ignore_label,
-                dice_class=MemoryEfficientSoftDiceLoss
-            )
+# class nnUNetTrainerDiceCELoss_noSmooth_weighted(nnUNetTrainer):
 
-        deep_supervision_scales = self._get_deep_supervision_scales()
-        weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
-        weights[-1] = 0
-        weights = weights / weights.sum()
+#     def _build_loss(self):
 
-        loss = DeepSupervisionWrapper(loss, weights)
-        return loss
+#         # use self.class_weights if defined
+#         if hasattr(self, "class_weights") and self.class_weights is not None:
+#             class_weights = torch.tensor(
+#                 self.class_weights,
+#                 dtype=torch.float32,
+#                 device=self.device
+#             )
+#             ce_kwargs = {'weight': class_weights}
+#         else:
+#             ce_kwargs = {}
 
+#         if self.label_manager.has_regions:
+#             loss = DC_and_BCE_loss(
+#                 ce_kwargs,
+#                 {
+#                     'batch_dice': self.configuration_manager.batch_dice,
+#                     'do_bg': True,
+#                     'smooth': 0,
+#                     'ddp': self.is_ddp
+#                 },
+#                 weight_ce=1,
+#                 weight_dice=1,
+#                 use_ignore_label=self.label_manager.ignore_label is not None,
+#                 dice_class=MemoryEfficientSoftDiceLoss
+#             )
 
-class nnUNetTrainerDiceCELoss_noSmooth_250epochs(nnUNetTrainerDiceCELoss_noSmooth):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
-                 device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-        self.num_epochs = 250
+#         else:
+#             loss = DC_and_CE_loss(
+#                 {
+#                     'batch_dice': self.configuration_manager.batch_dice,
+#                     'smooth': 0,
+#                     'do_bg': False,
+#                     'ddp': self.is_ddp
+#                 },
+#                 ce_kwargs,
+#                 weight_ce=1,
+#                 weight_dice=1,
+#                 ignore_label=self.label_manager.ignore_label,
+#                 dice_class=MemoryEfficientSoftDiceLoss
+#             )
 
+#         deep_supervision_scales = self._get_deep_supervision_scales()
+#         weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+#         weights[-1] = 0
+#         weights = weights / weights.sum()
 
-class nnUNetTrainerDiceCELoss_noSmooth_100epochs(nnUNetTrainerDiceCELoss_noSmooth):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
-                 device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-        self.num_epochs = 100
+#         loss = DeepSupervisionWrapper(loss, weights)
 
-
-class nnUNetTrainerDiceCELoss_noSmooth_2000epochs(nnUNetTrainerDiceCELoss_noSmooth):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
-                 device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-        self.num_epochs = 2000
+#         return loss
 
 
-class nnUNetTrainerDiceCELoss_noSmooth_4000epochs(nnUNetTrainerDiceCELoss_noSmooth):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
-                 device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-        self.num_epochs = 4000
+# class nnUNetTrainerDiceCELoss_noSmooth_weighted_250epochs(nnUNetTrainerDiceCELoss_noSmooth_weighted):
+#     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True, device: torch.device = torch.device('cuda')):
+
+#         super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+
+#         self.num_epochs = 250
+#         self.class_weights = [1.0, 1.0, 1.5]
 
 
-class nnUNetTrainerDiceCELoss_noSmooth_8000epochs(nnUNetTrainerDiceCELoss_noSmooth):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
-                 device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-        self.num_epochs = 8000
+# class nnUNetTrainer_DiceCE_Weighted(nnUNetTrainer):
+#     def _build_loss(self):
+#         if self.label_manager.has_regions:
+#             loss = DC_and_BCE_loss(
+#                 {},
+#                 {
+#                     'batch_dice': self.configuration_manager.batch_dice,
+#                     'do_bg': True,
+#                     'smooth': 1e-5,
+#                     'ddp': self.is_ddp
+#                 },
+#                 use_ignore_label=self.label_manager.ignore_label is not None,
+#                 dice_class=MemoryEfficientSoftDiceLoss
+#             )
+#         else:
+#             loss = DC_and_CE_loss(
+#                 {},
+#                 {
+#                     'batch_dice': self.configuration_manager.batch_dice,
+#                     'do_bg': False,
+#                     'smooth': 1e-5,
+#                     'ddp': self.is_ddp
+#                 },
+#                 weight_ce=0.7,
+#                 weight_dice=0.3,
+#                 ignore_label=self.label_manager.ignore_label,
+#                 dice_class=MemoryEfficientSoftDiceLoss
+#             )
+
+#         deep_supervision_scales = self._get_deep_supervision_scales()
+#         weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+#         weights[-1] = 0
+#         weights = weights / weights.sum()
+
+#         loss = DeepSupervisionWrapper(loss, weights)
+#         return loss
 
 
-class nnUNetTrainerDiceCELoss_noSmooth_2000epochs_NoMirroring(nnUNetTrainerDiceCELoss_noSmooth):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
-                 device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-        self.num_epochs = 2000
-
-    def configure_rotation_dummyDA_mirroring_and_inital_patch_size(self):
-        rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes = \
-            super().configure_rotation_dummyDA_mirroring_and_inital_patch_size()
-        mirror_axes = None
-        self.inference_allowed_mirroring_axes = None
-        return rotation_for_DA, do_dummy_2d_data_aug, initial_patch_size, mirror_axes
 
 
-class nnUNetTrainer_DiceCE_Weighted_250epochs(nnUNetTrainer_DiceCE_Weighted):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
-                 device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-        self.num_epochs = 250
 
 
-class nnUNetTrainer_DiceCE_Weighted_100epochs(nnUNetTrainer_DiceCE_Weighted):
-    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
-                 device: torch.device = torch.device('cuda')):
-        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-        self.num_epochs = 100
+
+
